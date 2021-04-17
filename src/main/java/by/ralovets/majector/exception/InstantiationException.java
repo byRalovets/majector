c12dae500da8ca34903dfc9666ef8da55d07e7db
Anton Ralovets <by.ralovets@gmail.com>
2021-04-17 21:07:18 +0300

    Add exception

---
+++ src/main/java/by/ralovets/majector/exception/InstantiationException.java
@@ -0,0 +1,12 @@
+package by.ralovets.majector.exception;
+
+public class InstantiationException extends RuntimeException {
+
+    public InstantiationException() {
+        super();
+    }
+
+    public InstantiationException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/by/ralovets/majector/exception/RecursiveInjectionException.java
@@ -0,0 +1,11 @@
+package by.ralovets.majector.exception;
+
+public class RecursiveInjectionException extends RuntimeException {
+
+    public RecursiveInjectionException() {
+    }
+
+    public RecursiveInjectionException(String message) {
+        super(message);
+    }
+}
